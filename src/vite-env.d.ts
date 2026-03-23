b59b8e4eea90474484826bf1536db96951e1dd0b
Valeriy <rikksandr@Gmail.com>
2026-03-23 17:20:44 +0300

    fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,25 +1,25 @@
 /// <reference types="vite/client" />
 
-interface TelegramWebApp {
-  ready: () => void;
-  expand: () => void;
-  close: () => void;
-  setHeaderColor: (color: string) => void;
-  setBackgroundColor: (color: string) => void;
-  themeParams: Record<string, string>;
-  viewportHeight: number;
-  viewportStableHeight: number;
-  isExpanded: boolean;
-  HapticFeedback: {
-    impactOccurred: (style: "light" | "medium" | "heavy") => void;
-    notificationOccurred: (type: "error" | "success" | "warning") => void;
-    selectionChanged: () => void;
-  };
-}
-
 export {};
 
 declare global {
+  interface TelegramWebApp {
+    ready: () => void;
+    expand: () => void;
+    close: () => void;
+    setHeaderColor: (color: string) => void;
+    setBackgroundColor: (color: string) => void;
+    themeParams: Record<string, string>;
+    viewportHeight: number;
+    viewportStableHeight: number;
+    isExpanded: boolean;
+    HapticFeedback: {
+      impactOccurred: (style: "light" | "medium" | "heavy") => void;
+      notificationOccurred: (type: "error" | "success" | "warning") => void;
+      selectionChanged: () => void;
+    };
+  }
+
   interface Window {
     Telegram?: { WebApp: TelegramWebApp };
   }
